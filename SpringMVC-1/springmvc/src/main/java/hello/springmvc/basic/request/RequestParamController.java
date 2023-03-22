48292be83d96023e6943da86f6c1ea99594aab8c
강지원 <kangji149@naver.com>
2023-03-22 15:34:53 +0900

    understand MVC

--- SpringMVC-1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ SpringMVC-1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,11 +1,10 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,5 +30,30 @@ public class RequestParamController {
     public String requestParmMap(@RequestParam Map<String, Object> paramMap) throws IOException {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
         return "ok";
+    }
+
+    @GetMapping("/model-attribute-v1")
+    @ResponseBody
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+
+        String username = helloData.getUsername();
+        int age = helloData.getAge();
+        log.info("HelloData = {}", helloData);
+
+        return "ok";
+    }
+
+    @PostMapping("/request-body-string-v4")
+    @ResponseBody//리턴을 메세지바디에 넣어준다
+    public String response(@RequestBody String message) {//요청메세지바디를 바로 파라미터로 받을수있다.
+        log.info("MessageBody = {}", message);
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) {
+        log.info("username = {} age = {}", data.getUsername(), data.getAge());
+        return data;
     }
 }
