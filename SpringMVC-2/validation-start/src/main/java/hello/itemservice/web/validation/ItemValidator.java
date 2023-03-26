a06dcf2bde580b8c7e2c821756425a52a58ac1b1
강지원 <kangji149@naver.com>
2023-03-26 15:09:25 +0900

    Bean Validation

--- SpringMVC-2/validation-start/src/main/java/hello/itemservice/web/validation/ItemValidator.java
+++ SpringMVC-2/validation-start/src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
@@ -17,24 +18,28 @@ public class ItemValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Item item = (Item) target;
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemName", "required");
-//        if (!StringUtils.hasText(item.getItemName())) {
-//            errors.rejectValue("itemName", "required");
-//        }
 
-        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            errors.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        if (errors.hasErrors()) {
+            return;
+        }
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.rejectValue("itemName", "required");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 10000 || item.getPrice() > 1000000) {
+            errors.rejectValue("price", "range", new Object[]{10000, 1000000}, null);
         }
 
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             errors.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
 
-        if (item.getPrice() != null  && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int result = item.getPrice() * item.getQuantity();
 
-            if (resultPrice < 10000) {
-                errors.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            if (result < 10000) {
+                errors.reject("totalPriceMin", new Object[]{10000, result}, null);
             }
         }
     }
--- SpringMVC-2/validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ SpringMVC-2/validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -6,56 +6,44 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Controller
-@RequestMapping("/validation/v2/items")
+@RequestMapping("/validation/v3/items")
 @RequiredArgsConstructor
-public class ValidationItemControllerV2 {
+public class ValidationItemControllerV3 {
 
-    @InitBinder
-    void init(WebDataBinder dataBinder) {
-        log.info("dataBinder = {}", dataBinder);
-        dataBinder.addValidators(itemValidator);
-    }//컨트롤러가 호출때마다 매번 호출 -> 매번 dataBinder에 Validator를 등록
     private final ItemRepository itemRepository;
-    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "validation/v2/items";
+        return "validation/v3/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v2/item";
+        return "validation/v3/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());//재사용성
-        return "validation/v2/addForm";
+        return "validation/v3/addForm";
     }
 
     @PostMapping("/add")
-    public String addItemV3(@Validated @ModelAttribute Item item, /*@Validator 애노테이션이 붙어있으면 Validator가 등록되어있는지
+    public String addItem(@Validated @ModelAttribute Item item, /*@Validator 애노테이션이 붙어있으면 Validator가 등록되어있는지
                                                                     확인하고 다 뒤져서 supports를 통해 검증가능한 Validator가 있다면 검증실행*/
                             BindingResult bindingResult,//Map errors의 역할
                             RedirectAttributes redirectAttributes,
@@ -63,26 +51,26 @@ public class ValidationItemControllerV2 {
 
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
-            return "validation/v2/addForm";
+            return "validation/v3/addForm";
         }
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v2/items/{itemId}";
+        return "redirect:/validation/v3/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v2/editForm";
+        return "validation/v3/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
-        return "redirect:/validation/v2/items/{itemId}";
+        return "redirect:/validation/v3/items/{itemId}";
     }
 
 }
