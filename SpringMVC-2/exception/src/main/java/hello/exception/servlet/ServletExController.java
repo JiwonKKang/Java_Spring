f1c46b84d51b8657d3638c0ffb94c49182c9cd20
강지원 <kangji149@naver.com>
2023-03-30 23:57:53 +0900

    exception flow

--- SpringMVC-2/exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ SpringMVC-2/exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -1,8 +1,11 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
 
 @Controller
 @Slf4j
@@ -12,5 +15,14 @@ public class ServletExController {
     public void errorEx() {
         throw new RuntimeException("예외발생!");
     }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404오류 발생!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
 }
- 
