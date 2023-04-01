1f8caf9e38210cee7a990741100e54c726dd7d8f
강지원 <kangji149@naver.com>
2023-04-01 17:52:35 +0900

    Formatter

--- SpringMVC-2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ SpringMVC-2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -2,6 +2,7 @@ package hello.typeconverter;
 
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.fommater.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,5 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+        registry.addFormatter(new MyNumberFormatter());
+
+        //컨버터 우선순위 > 포맷터 우선순위
     }
 }
--- SpringMVC-2/typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
+++ SpringMVC-2/typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
@@ -2,6 +2,7 @@ package hello.typeconverter.converter;
 
 import hello.typeconverter.IpPort;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.support.DefaultConversionService;
 
 public class StringToIpPortConverter implements Converter<String, IpPort> {
     @Override
@@ -11,4 +12,6 @@ public class StringToIpPortConverter implements Converter<String, IpPort> {
         int port = Integer.parseInt(split[1]);
         return new IpPort(ip, port);
     }
+
+
 }
