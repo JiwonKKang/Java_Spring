3bd8e2f29f391efa75d16785044768dae90877d4
강지원 <kangji149@naver.com>
2023-04-03 18:47:50 +0900

    DB init

---
+++ SpringDB-1/jdvc/src/main/java/hello/jdvc/JdvcApplication.java
@@ -0,0 +1,13 @@
+package hello.jdvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdvcApplication.class, args);
+	}
+
+}
---
+++ SpringDB-1/jdvc/src/test/java/hello/jdvc/JdvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.jdvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
