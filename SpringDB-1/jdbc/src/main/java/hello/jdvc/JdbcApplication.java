37e8854836996699c531c65610d7fd5dc7ae7fda
강지원 <kangji149@naver.com>
2023-04-03 18:52:56 +0900

    DB init

--- SpringDB-1/jdbc/src/main/java/hello/jdvc/JdbcApplication.java
+++ SpringDB-1/jdbc/src/main/java/hello/jdvc/JdbcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JdvcApplication {
+public class JdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JdvcApplication.class, args);
+		SpringApplication.run(JdbcApplication.class, args);
 	}
 
 }
