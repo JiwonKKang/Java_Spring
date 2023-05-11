4a2799846b259858a2d78740691ae53564a954e4
강지원 <kangji149@naver.com>
2023-05-11 19:36:50 +0900

    트랜잭션 주의사항

---
+++ SpringDB-2/springtx/src/test/java/hello/springtx/apply/InitTxTest.java
@@ -0,0 +1,48 @@
+package hello.springtx.apply;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@SpringBootTest
+public class InitTxTest {
+
+    Hello hello;
+
+    @Autowired
+    public InitTxTest(Hello hello) {
+        this.hello = hello;
+    }
+
+    @Test
+    void go() {
+    }
+
+    @TestConfiguration
+    static class InitTxTestConfig {
+        @Bean
+        Hello hello() {
+            return new Hello();
+        }
+    }
+
+    static class Hello {
+
+        @PostConstruct
+        @Transactional
+        public void initV1() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @PostConstruct tx active={}", isActive);
+        }
+    }
+
+}
